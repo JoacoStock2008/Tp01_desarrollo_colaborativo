06dcc38da1a7c1e0de437febbae21ee5a555d2ef
JoacoStock2008 <joacostock2008@gmail.com>
2026-03-20 16:20:21 -0300

    agregando contador

--- script.js
+++ script.js
@@ -3,30 +3,44 @@ let votos = 0
 const boton = document.getElementById("votar")
 const textoVotos = document.getElementById("contador")
 
-boton.addEventListener("click", function(){
+boton.addEventListener("click", function () {
 
-votos += 1
+    votos += 1
 
-textoVotos.innerHTML = "Votos: " + votos
+    textoVotos.innerHTML = "Votos: " + votos
 
 })
 
-function VerificarNombre(){
+function VerificarNombre() {
     let nombre = document.getElementById("nombre").value
     let nombreArray = nombre//seguir
 }
 const form = document.getElementById("formPropuesta")
 
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", function () {
 
-let nombre = document.getElementById("nombre")
-let idea = document.getElementById("idea")
+    let nombre = document.getElementById("nombre")
+    let idea = document.getElementById("idea")
 
-if(nombre.value == "" || idea.value == ""){
+    if (nombre.value == "" || idea.value == "") {
 
-document.getElementById("mensaje").innerHTML = "Completa todos los campos"
+        document.getElementById("mensaje").innerHTML = "Complete todos los campos"
 
-}
+    } else {
+        let tiempo = 5;
+        const intervalo = setInterval(function () {
+            document.getElementById("mensaje").innerHTML = "La propuesta se ha enviado correctamente. Redirigiendo en " + tiempo + " segundos..."
+            tiempo -= 1;
+
+            if(tiempo < 0){
+                clearInterval(intervalo)
+                nombre.value = ""
+                idea.value = ""
+                document.getElementById("mensaje").innerHTML = "Complete el formulario nuevamente para enviar otra propuesta"
+
+            }
+        }, 1000)
+    }
 
 
 })
